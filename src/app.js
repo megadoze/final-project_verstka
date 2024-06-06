7a05dafd79f4ca3e899503b1cce4f4000ab88a54
Oleg <Oleg@macs-MBP-4.fritz.box>
2024-06-07 01:35:22 +0200

    add testimonial section

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import Swiper from "swiper";
+import { Pagination, A11y } from "swiper/modules";
+
 // Ждем пока закрузится вся страница
 document.addEventListener("DOMContentLoaded", () => {
   // Menu
@@ -16,16 +19,43 @@ document.addEventListener("DOMContentLoaded", () => {
       button.setAttribute("aria-expanded", "true");
     }
   };
-});
 
-// Video
-const videoButton = document.querySelector(".js-video-play");
-videoButton.onclick = function () {
-  const video = videoButton.parentElement;
-  const videoIframe = document.querySelector("iframe");
-  videoIframe.contentWindow.postMessage(
-    JSON.stringify({ event: "command", func: "playVideo" }),
-    "*"
-  );
-  video.classList.add("is-active");
-};
+  // Video
+  const videoButton = document.querySelector(".js-video-play");
+  videoButton.onclick = function () {
+    const video = videoButton.parentElement;
+    const videoIframe = document.querySelector("iframe");
+    videoIframe.contentWindow.postMessage(
+      JSON.stringify({ event: "command", func: "playVideo" }),
+      "*"
+    );
+    video.classList.add("is-active");
+  };
+
+  // Testimonials
+  const swiper = new Swiper(".js-testimonials-slider", {
+    // configure Swiper to use modules
+    modules: [Pagination, A11y],
+    loop: true,
+    speed: 600,
+    // autoHeight: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    a11y: true,
+    pagination: {
+      el: ".testimonials-slider__pagination",
+      bulletElement: "button",
+      bulletClass: "testimonials-slider__pagination-bullet",
+      bulletActiveClass: "testimonials-slider__pagination-bullet--active",
+      clickable: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 0,
+        slideToClickedSlide: true,
+      },
+    },
+  });
+});
